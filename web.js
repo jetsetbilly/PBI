51336900bb58501d1056c01bdcb0ea7c8c057dac
jetsetbilly <grkyle@gmail.com>
2013-09-22 18:04:42 +0000

    added web.js

---
+++ web.js
@@ -0,0 +1,16 @@
+var fs = require('fs');
+
+var express = require('express');
+var app = express();
+app.use(express.logger());
+
+app.get('/', function(request, response) {
+  var k = fs.readFileSync('index.html');
+  var text = k.toString('ascii');
+ response.send( text );
+});
+
+var port = process.env.PORT || 8080;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
