2ce2db4e1b93508e3d257f003808ed917c5067dc
jetsetbilly <grkyle@gmail.com>
2013-09-22 18:49:04 +0000

    changed web.js web server

--- web.js
+++ web.js
@@ -1,16 +1,9 @@
 var fs = require('fs');
-
 var express = require('express');
 var app = express();
-app.use(express.logger());
-
-app.get('/', function(request, response) {
-  var k = fs.readFileSync('index.html');
-  var text = k.toString('ascii');
- response.send( text );
+app.configure(function () {
+	app.use(
+		"/", express.static(__dirname)
+	);
 });
-
-var port = process.env.PORT || 8080;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+app.listen(8080);
